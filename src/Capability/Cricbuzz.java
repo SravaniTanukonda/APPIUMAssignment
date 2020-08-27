55dc3d40a734edffea6684f77c4bea132f6a6c69
SravaniTanukonda <stanukon@in.ibm.com>
2020-08-27 17:54:30 +0530

    Second Commit

--- src/Capability/Cricbuzz.java
+++ src/Capability/Cricbuzz.java
@@ -19,15 +19,15 @@ public class Cricbuzz {
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Sravani");
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
 		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-	
-		cap.setCapability(AndroidMobileCapabilityType.CHROMEDRIVER_EXECUTABLE, "C:\\Users\\SravaniTanukonda\\Downloads\\chromedriver_win32(2)\\chromedriver.exe");
-		AndroidDriver<AndroidElement> driver=new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"), cap);
+	    cap.setCapability(AndroidMobileCapabilityType.CHROMEDRIVER_EXECUTABLE, "C:\\Users\\SravaniTanukonda\\Downloads\\chromedriver_win32(2)\\chromedriver.exe");
+		AndroidDriver<AndroidElement>driver=new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"), cap);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("m.cricbuzz.com");
-	driver.findElement(MobileBy.className("android.widget.TextView")).click();
-		driver.findElementByAndroidUIAutomator("text(\"HOME\")").click();
-	WebElement Link = driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"England clinch series 1-0\"));");
-	Link.click();	
+	    //driver.findElement(MobileBy.className("android.widget.TextView")).click();
+	   /* driver.findElementByAndroidUIAutomator("text(\"Menu\")").click();
+        driver.findElementByAndroidUIAutomator("text(\"HOME\")").click();
+	    WebElement Link = driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"England clinch series 1-0\"));");
+	    Link.click();*/
 	
 	}
 }
